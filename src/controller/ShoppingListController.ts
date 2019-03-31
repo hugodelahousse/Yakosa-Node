9502a1d8d5b904ef64542903218a453f52f561f3
Hugo Delahousse <hugo.delahousse@epita.fr>
2019-03-31 18:35:45 +0200

    ShoppingListController: Add Patch and Post

--- src/controller/ShoppingListController.ts
+++ src/controller/ShoppingListController.ts
@@ -1,5 +1,12 @@
 import { getRepository } from 'typeorm';
-import { Body, Delete, Get, JsonController, OnUndefined, Param, Post } from 'routing-controllers';
+import {
+  BadRequestError,
+  Body,
+  Get, HttpCode,
+  JsonController,
+  Param, Patch,
+  Post,
+} from 'routing-controllers';
 import ShoppingList from '@entities/ShoppingList';
 
 @JsonController()
@@ -12,8 +19,34 @@ export class ShoppingListController {
     return this.repository.find();
   }
 
+  @Get('/lists/:id')
+  async one(@Param('id') id: number) {
+    return this.repository.findOne(id, { relations: ['products', 'products.product'] });
+  }
+
   @Get('/lists/for/:userId')
   async allForUser(@Param('userId') userId: number) {
     return this.repository.find({ userId });
+  }
+
+  @Post('/lists/')
+  @HttpCode(201)
+  async create(@Body() list: ShoppingList) {
+    try {
+      return await this.repository.save(list);
+    } catch (e) {
+      throw new BadRequestError(e.detail);
+    }
+  }
+
+  @Patch('/lists/:id/')
+  async patch(@Param('id') id: number, @Body() list: ShoppingList) {
+    const existing = await this.repository.findOne(id);
+    if (existing === undefined) {
+      return undefined;
+    }
+    existing.creationDate = list.creationDate || existing.creationDate;
+    existing.lastUsed = list.lastUsed || existing.lastUsed;
+    return existing;
   }
 }
