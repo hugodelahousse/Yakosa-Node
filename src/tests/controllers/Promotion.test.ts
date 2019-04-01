fc06e4caf2ee55cf354d1f0d65d8d1b2c8937564
Théo Issarni <issarnitheo@gmail.com>
2019-04-01 18:33:29 +0200

    test: add store tests

--- src/tests/controllers/Promotion.test.ts
+++ src/tests/controllers/Promotion.test.ts
@@ -1,46 +1,47 @@
+import createApp from '@utils/createApp';
 import * as chai from 'chai';
 import chaiHttp = require('chai-http');
-import { app, lists, users } from './setup';
+import { app, promotions, users } from './setup';
 
 chai.use(chaiHttp);
 
 const expect = chai.expect;
 
-describe('ShoppingListController should be able to list items', () => {
+describe('PromotionController should be able to list items', () => {
   it('Should respond with 200', async () => {
     const res = await chai.request(app).get('/lists/');
     expect(res).to.have.status(200);
   });
 
-  it('Should list existing shopping lists', async () => {
-    const res = await chai.request(app).get('/lists/');
+  it('Should list existing promotions', async () => {
+    const res = await chai.request(app).get('/promotions/');
     expect(res).to.be.json;
     expect(res.body).to.have.length.above(0);
-    expect(res.body).to.have.length(lists.length);
-    const dbIds = lists.map(list => list.id);
+    expect(res.body).to.have.length(promotions.length);
+    const dbIds = promotions.map(list => list.id);
     const responseIds = res.body.map(list => list.id);
     expect(responseIds).to.have.members(dbIds);
   });
 
   it('Should display user ids', async () => {
-    const res = await chai.request(app).get('/lists/');
-    const dbIds = lists.map(list => list.userId);
+    const res = await chai.request(app).get('/promotions/');
+    const dbIds = promotions.map(list => list.userId);
     const responseIds = res.body.map(list => list.userId);
     expect(responseIds).to.have.members(dbIds);
   });
 });
 
-describe('ShoppingListController should be able to find lists for a specific user', () => {
+describe('PromotionsController should be able to find lists for a specific user', () => {
   it('Responds with 200', async () => {
     const user = users[0];
-    const res = await chai.request(app).get(`/lists/for/${user.id}/`);
+    const res = await chai.request(app).get(`/promotions/for/${user.id}/`);
     expect(res).to.have.status(200);
   });
 
   it('Returns the proper lists', async () => {
     const user = users[0];
-    const res = await chai.request(app).get(`/lists/for/${user.id}/`);
-    const userLists = user.shoppingLists;
+    const res = await chai.request(app).get(`/promotions/for/${user.id}/`);
+    const userLists = user.postedPromotions;
     expect(res.body).to.have.length(userLists.length);
     expect(userLists.map(l => l.id)).to.have.members(res.body.map(l => l.id));
   });
