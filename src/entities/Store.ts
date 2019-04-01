e156008203358481a9eba9e48a63208f607cbd36
Théo Issarni <issarnitheo@gmail.com>
2019-04-01 14:43:18 +0200

    test: fixture update yml files

--- src/entities/Store.ts
+++ src/entities/Store.ts
@@ -23,16 +23,3 @@ export class Store {
 
 }
 
-/*
-{
-  "brand": 1,
-  "id": 22,
-  "position": {
-      "coordinates": [
-          139.9341032213472,
-          36.80798008559315
-       ],
-       "type": "Point"
-   }
-}
-*/
