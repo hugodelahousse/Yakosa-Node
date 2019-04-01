e950df44b9ca3f8d6b27d2dedb577037013723ce
Théo Issarni <issarnitheo@gmail.com>
2019-04-01 18:33:29 +0200

    test: add store tests

--- src/tests/controllers/setup.ts
+++ src/tests/controllers/setup.ts
@@ -5,18 +5,22 @@ import { getRepository } from 'typeorm';
 import ShoppingList from '@entities/ShoppingList';
 import { User } from '@entities/User';
 import { Store } from '@entities/Store';
+import { Promotion } from '@entities/Promotion';
 
 export let app;
 
 export let lists: ShoppingList[];
 export let stores: Store[];
 export let users: User[];
+export let promotions: Promotion[];
 
 before(async () => {
   app = await createApp();
   this.connection = await createTypeormConnection();
-  await loadFixtures('User.yml', 'ShoppingList.yml', 'Brand.yml', 'Store.yml');
+  await loadFixtures('User.yml', 'ShoppingList.yml', 'Brand.yml', 'Store.yml', 'Product.yml',
+                     'Promotion.yml');
   lists = await getRepository(ShoppingList).find();
   stores = await getRepository(Store).find();
-  users = await getRepository(User).find({ relations: ['shoppingLists'] });
+  users = await getRepository(User).find({ relations: ['shoppingLists', 'postedPromotions'] });
+  promotions = await getRepository(Promotion).find() ;
 });
