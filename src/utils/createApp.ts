ab863d5f401e1240a552cc75da599be2c5543463
Romain Mounier <romain.mounier@epita.fr>
2019-04-04 20:39:48 +0200

    productController : add get all

--- src/utils/createApp.ts
+++ src/utils/createApp.ts
@@ -5,13 +5,19 @@ import { UserController } from '../controller/UserController';
 import { ShoppingListController } from '../controller/ShoppingListController';
 import { StoreController } from '../controller/StoreController';
 import { PromotionController } from '../controller/PromotionController';
+import { ProductController} from '../controller/ProductController';
 import createTypeormConnection from './createTypeormConnection';
 
 export default async function createApp() {
 
   const app = express();
   useExpressServer(app, {
-    controllers: [UserController, ShoppingListController, StoreController, PromotionController],
+    controllers: [
+      UserController,
+      ShoppingListController,
+      StoreController,
+      PromotionController,
+      ProductController],
   });
   app.use(bodyParser.json());
 
