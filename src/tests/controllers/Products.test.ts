ac24bce9d6541851de1a877d4378499188088e75
Romain Mounier <romain.mounier@epita.fr>
2019-04-22 12:03:09 +0200

    correctly fixed the product tests

--- src/tests/controllers/Products.test.ts
+++ src/tests/controllers/Products.test.ts
@@ -10,7 +10,7 @@ describe('produits can list items', () => {
   it('should list existing products in the database', async () => {
     const res = await chai.request(app).get('/products/');
     expect(res).to.be.json;
-    expect(res).to.have.length.above(0);
-    expect(res).to.have.least(products.length)
+    expect(res.body).to.have.length.above(0);
+    expect(res.body).to.have.length(products.length)
   })
 })
