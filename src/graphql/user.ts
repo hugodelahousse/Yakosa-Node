33663cf06479d10f3a09ad25a9beb477e0756aa9
Hugo Delahousse <hugo.delahousse@epita.fr>
2019-04-23 12:10:36 +0200

    GraphQL: Add utils

--- src/graphql/user.ts
+++ src/graphql/user.ts
@@ -1,10 +1,11 @@
 import { gql } from 'apollo-server-express';
-import { FindManyOptions, getRepository } from 'typeorm';
+import { getRepository } from 'typeorm';
 import { User } from '@entities/User';
 import ShoppingList from '@entities/ShoppingList';
+import { graphQLFindList } from '@graphql/utils';
 
 export const typeDefs = gql`
-  type User {
+  type User @UUID {
       id: ID!
       firstName: String!
       lastName: String!
@@ -21,20 +22,14 @@ export const typeDefs = gql`
 
 export const resolvers = {
   Query: {
-    allUsers: async (parent, args, context, info) => {
-      console.log(parent, args, context, info);
-      const options: FindManyOptions = {};
-      if (args.offset !== undefined) { options.skip = args.offset; }
-      if (args.limit !== undefined) { options.take = args.limit; }
-      return await getRepository(User).find(options);
-    },
+    allUsers: async (parent, args, context, info) =>
+      graphQLFindList(User, args),
     user: async (parent, args) => {
       return await getRepository(User).findOne(args.id);
     },
   },
   User: {
-    shoppingLists: async (parent: User) => {
-      return await getRepository(ShoppingList).find({ userId: parent.id });
-    },
+    shoppingLists: async (parent: User, args) =>
+      graphQLFindList(ShoppingList, args, { userId: parent.id }),
   },
 };
