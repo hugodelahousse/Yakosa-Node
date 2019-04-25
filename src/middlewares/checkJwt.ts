78c5db44c789f76bd5413aae6f7b8a0074ae2ec9
Adrien Hellec <adrien.hellec@epita.fr>
2019-04-25 15:58:29 +0200

    jwt: add refresh token and google token verification

--- src/middlewares/checkJwt.ts
+++ src/middlewares/checkJwt.ts
@@ -71,26 +71,43 @@ export async function checkJwt(request: Request, response: Response, next: NextF
 =======
 import * as jwt from 'jsonwebtoken'
 import config from 'config'
+import crypto = require('crypto');
+
+export var refreshTokens = {};
 
 export function checkJwt(request: Request, response: Response, next: NextFunction) {
     const token = <string>request.headers.authorization;
-    let jwtPayload;
-    
+    const refresh = <string>request.headers['refresh'];  
     //Verification of token
     try {
-        jwtPayload = <any>jwt.verify(token, config.JWT_SECRET);
-        response.locals.jwtPayload = jwtPayload;
+        jwt.verify(token, config.JWT_SECRET);
     } catch (error) {
-        response.status(401).send({ status: 401, error: 'Token verification failed'});
-        return;
+        if (error.name !== 'TokenExpiredError')
+            return response.status(401).send({ status: 401, error: error.message});
     }
+
+    var jwtDecoded = (jwt.decode(token) as any);
+    if (!jwtDecoded)
+        return response.status(401).send({ status: 401, error: 'Token verification failed.'});
+
+    if (refresh !== undefined && refresh.length > 0) {
+        if((refresh in refreshTokens) && (refreshTokens[refresh] == jwtDecoded.userId)) {
+            const newToken = jwt.sign({ userId: jwtDecoded.userId, googleId: jwtDecoded.googleId },
+                config.JWT_SECRET, { expiresIn: 1800 },
+            );
+            const newRefresh = crypto.randomBytes(Math.ceil(128 / 2)).toString('hex').slice(0, 128);
+            delete refreshTokens[refresh];
+            refreshTokens[newRefresh] = jwtDecoded.userId;
+            return response.send({ token: newToken, refresh: newRefresh, googleId: jwtDecoded.googleId });
+        }
+        else {
+            return response.status(401).send({ status: 401, error: 'Refresh Token verification failed'});
+        }
+    }
+    const expirationDate = new Date(jwtDecoded.exp * 1000);
+    if (expirationDate < new Date())
+        return response.status(401).send({ status: 401, error: 'Token expired. Use refresh token to generate one.'});
   
-    //New token of 1h on every requests
-    const { userId, googleId } = jwtPayload;
-    const newToken = jwt.sign({ userId, googleId }, config.JWT_SECRET, {
-        expiresIn: '1h'
-    });
-    response.setHeader('token', newToken);
     next();
   };
 >>>>>>> jwt: add middleware and token creation
