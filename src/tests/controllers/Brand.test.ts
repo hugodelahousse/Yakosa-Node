60ea73df63c3a4fade11767c5255472ebb824dd4
Romain Mounier <romain.mounier@epita.fr>
2019-05-02 13:42:11 +0200

    fix test

--- src/tests/controllers/Brand.test.ts
+++ src/tests/controllers/Brand.test.ts
@@ -1,6 +1,6 @@
 import * as chai from 'chai';
 import chaiHttp = require('chai-http');
-import { app, brands } from './setup';
+import { app, brands } from '../setup';
 
 chai.use(chaiHttp);
 
@@ -8,14 +8,12 @@ const expect = chai.expect;
 
 describe('PromotionController GET', () => {
   it('Should respond with 200', async () => {
-    const res = await chai.request(app)
-        .get('/brands');
+    const res = await chai.request(app).get('/brands');
     expect(res).to.have.status(200);
   });
 
   it('Should return existing stores', async () => {
-    const res = await chai.request(app)
-        .get('/brands');
+    const res = await chai.request(app).get('/brands');
     expect(res).to.be.json;
     expect(res.body).to.have.length.above(0);
     expect(res.body).to.have.length(brands.length);
