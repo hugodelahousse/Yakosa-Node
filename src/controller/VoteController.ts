30a1f1c1dcfeebd9176cd655220ff82088339ae0
Romain Mounier <romain.mounier@epita.fr>
2019-05-11 11:13:22 +0200

    add right on vote

--- src/controller/VoteController.ts
+++ src/controller/VoteController.ts
@@ -1,20 +1,29 @@
 import { getRepository } from 'typeorm';
-import { Body, Delete, Get, JsonController, OnUndefined, Param, Post, Patch, HttpCode } from 'routing-controllers';
+import {
+  Body,
+  Delete,
+  Get,
+  JsonController,
+  OnUndefined,
+  Param,
+  Post,
+  Patch,
+  HttpCode,
+} from 'routing-controllers';
 import { Vote } from '@entities/Vote';
 
 @JsonController()
 export class VoteController {
-
-  private voteRepository = getRepository(Vote);
+  private repository = getRepository(Vote);
 
   @Get('/votes/')
   async all() {
-    return await this.voteRepository.find();
+    return await this.repository.find();
   }
 
   @Get('/votes/:id')
   async one(@Param('id') id: number) {
-    return await this.voteRepository.findOne(id, {
+    return await this.repository.findOne(id, {
       relations: ['user', 'promotion'],
     });
   }
@@ -22,33 +31,38 @@ export class VoteController {
   @Post('/votes/')
   @HttpCode(201)
   async save(@Body() vote: Vote) {
-    return await this.voteRepository.save(vote);
+    return await this.repository.save(vote);
   }
 
   @OnUndefined(404)
   @Delete('/votes/:id')
   async remove(@Param('id') id: number) {
-    const voteToRemove = await this.voteRepository.findOne(id);
+    const voteToRemove = await this.repository.findOne(id);
     if (voteToRemove) {
-      await this.voteRepository.remove(voteToRemove);
+      await this.repository.remove(voteToRemove);
     }
     return voteToRemove;
   }
 
   @OnUndefined(404)
   @Patch('/votes/:id')
-  async update(@Param('id') id: number,
-               @Body() vote: Vote) {
-    const existing = await this.voteRepository.findOne(id);
+  async update(@Param('id') id: number, @Body() vote: Vote) {
+    const existing = await this.repository.findOne(id);
     if (existing === undefined) {
       return undefined;
     }
     const fieldsToChange = ['upvote', 'user', 'promotion'];
     for (let i = 0; i < fieldsToChange.length; i += 1) {
       const field = fieldsToChange[i];
-      if (vote.hasOwnProperty(field)) { existing[field] = vote[field]; }
+      if (vote.hasOwnProperty(field)) {
+        existing[field] = vote[field];
+      }
     }
-    return await this.voteRepository.save(existing);
+    return await this.repository.save(existing);
   }
 
+  async hasUserRight(userId: number, voteId: number) {
+    const vote = await this.repository.findOne(voteId);
+    return vote && vote.userId == userId;
+  }
 }
