9951f27e182504db7b120f87a8adadd8895a2d0c
Romain Mounier <romain.mounier@epita.fr>
2019-06-16 17:18:15 +0200

    add comment to the getShopValue function

--- src/utils/CreateShoppingRoute.ts
+++ src/utils/CreateShoppingRoute.ts
@@ -3,13 +3,21 @@ import { Store } from '@entities/Store';
 import { Promotion } from '@entities/Promotion';
 import { StoreWithValueAndPromotion } from 'types/ShoppingRoute';
 
+/**
+ * This function calculated the value of a shop for our algo
+ * @param shoppingList
+ * @param store
+ */
 export function getShopValue(
   shoppingList: ShoppingList,
   store: Store,
 ): StoreWithValueAndPromotion {
-  let score = 0;
-  const promotionsChoice: Promotion[] = [];
+  let score = 0; // total economy on this shop
+  const promotionsChoice: Promotion[] = []; // list of the promotion choosen in this shop
   shoppingList.products.forEach(product => {
+    // for each product in the shopping list we choose the
+    // best promotion related to it
+
     let actualPromo = 0;
     let promoChosen: Promotion | null = null;
 
@@ -22,17 +30,24 @@ export function getShopValue(
         actualPromo = promotion.promotion * product.quantity;
       }
     };
+
+    // we search in the promotions of the shop
     store.promotions.filter(filterToRelatedPromo).forEach(findBestPromo);
     if (store.brand && store.brand.promotions) {
+      // and in the promotion of the brand
       store.brand.promotions
         .filter(filterToRelatedPromo)
         .forEach(findBestPromo);
     }
     if (actualPromo > 0) {
+      // If we found at least one promotion related to the product we add it
+      // to our total
       score += actualPromo;
       promotionsChoice.push(promoChosen!);
     }
   });
+  // The struct returned containes all our result
+  // And will be usefull for optimisation
   return {
     Store: store,
     value: score,
