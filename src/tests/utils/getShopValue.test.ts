ad2a12827739d1ebedf2bffb8648113a6cc2522b
Romain Mounier <romain.mounier@epita.fr>
2019-06-16 16:45:56 +0200

    GetShopValue : add test to choose best promotion between multiple promotion in a shop

--- src/tests/utils/getShopValue.test.ts
+++ src/tests/utils/getShopValue.test.ts
@@ -11,60 +11,87 @@ import { expect } from 'chai';
 import { from } from 'apollo-link';
 
 describe('should create score associate with promotion', () => {
-  it('should select the only promotion', async () => {
+  const product1: Product = {
+    barcode: '12345',
+    promotions: [],
+    listProducts: [],
+  };
+
+  const store1: Store = {
+    id: 1,
+    position: '',
+    brand: new Brand(),
+    brandId: 0,
+    promotions: [],
+    managers: [],
+    managersId: [],
+  };
+
+  const promotion1: Promotion = {
+    id: 1,
+    description: '',
+    price: 2,
+    promotion: 1,
+    beginDate: new Date(),
+    endDate: new Date(),
+    type: 1,
+    userId: 0,
+    storeId: 0,
+    brandId: 0,
+    product: product1,
+    user: new User(),
+    brand: new Brand(),
+    store: store1,
+    votes: [],
+  };
+
+  const promotion2: Promotion = {
+    id: 1,
+    description: '',
+    price: 2,
+    promotion: 1.5,
+    beginDate: new Date(),
+    endDate: new Date(),
+    type: 1,
+    userId: 0,
+    storeId: 0,
+    brandId: 0,
+    product: product1,
+    user: new User(),
+    brand: new Brand(),
+    store: store1,
+    votes: [],
+  };
+  store1.promotions.push(promotion1);
+
+  const shoppingList1 = new ShoppingList();
+  shoppingList1.products = [];
+  const listProduct1: ListProduct = {
+    id: 1,
+    quantity: 1,
+    list: shoppingList1,
+    listId: 0,
+    product: product1,
+    productBarcode: '12345',
+  };
+  shoppingList1.products.push(listProduct1);
+
+  it('should select the only promotion', () => {
     // Given
 
-    const product: Product = {
-      barcode: '12345',
-      promotions: [],
-      listProducts: [],
-    };
-
-    const store: Store = {
-      id: 1,
-      position: '',
-      brand: new Brand(),
-      brandId: 0,
-      promotions: [],
-      managers: [],
-      managersId: [],
-    };
-
-    const promotion: Promotion = {
-      id: 1,
-      description: '',
-      price: 2,
-      promotion: 1,
-      beginDate: new Date(),
-      endDate: new Date(),
-      type: 1,
-      userId: 0,
-      storeId: 0,
-      brandId: 0,
-      product,
-      user: new User(),
-      brand: new Brand(),
-      store,
-      votes: [],
-    };
-    store.promotions.push(promotion);
-
-    const shoppingList = new ShoppingList();
-    shoppingList.products = [];
-    const listProduct: ListProduct = {
-      id: 1,
-      quantity: 1,
-      list: shoppingList,
-      listId: 0,
-      product,
-      productBarcode: '12345',
-    };
-    shoppingList.products.push(listProduct);
-
-    const result = getShopValue(shoppingList, store);
+    const result = getShopValue(shoppingList1, store1);
     expect(result.value).to.be.equal(1);
     expect(result.Promotions).to.have.length.above(0);
     expect(result.Promotions.length).to.be.equal(1);
-    expect(result.Promotions[0]).to.be.equal(promotion);
+    expect(result.Promotions[0]).to.be.equal(promotion1);
+  });
+
+  it('should select the best promotion between the 2', () => {
+    store1.promotions.push(promotion2);
+    const result = getShopValue(shoppingList1, store1);
+    expect(result.value).to.be.equal(1.5);
+    expect(result.Promotions).to.have.length.above(0);
+    expect(result.Promotions.length).to.be.equal(1);
+    expect(result.Promotions[0]).to.be.equal(promotion2);
   });
 });
