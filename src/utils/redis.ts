5916c6a656ceeb70e4da5c4c48c6b02c15a723ce
Romain Mounier <romain.mounier@epita.fr>
2019-06-17 16:27:00 +0200

    reddis will just create log on error instead of throwing exception

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -2,3 +2,7 @@ import * as asyncRedis from 'async-redis';
 import config from 'config';
 
 export const redis = asyncRedis.createClient(config.REDIS_URL);
+
+redis.on('error', err => {
+  console.log(err);
+});
