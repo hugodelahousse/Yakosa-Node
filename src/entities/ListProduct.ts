f5f629101abd8ecd1203333c959acbd6f88d3b4d
Théo Issarni <issarnitheo@gmail.com>
2019-10-06 16:36:22 +0200

    promotion: add unit and quantity field

--- src/entities/ListProduct.ts
+++ src/entities/ListProduct.ts
@@ -8,14 +8,7 @@ import {
 } from 'typeorm';
 import ShoppingList from './ShoppingList';
 import { Product } from './Product';
-
-enum MesuringUnits {
-  UNIT = 0,
-  GRAMME = 1,
-  KILOGRAM = 2,
-  LITRE = 3,
-  CENTILITRE = 4,
-}
+import { MesuringUnits } from '@entities/utils';
 
 @Entity()
 @Unique(['list', 'product'])
